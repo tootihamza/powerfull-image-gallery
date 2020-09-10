fe55f87096dfb67fa3e13e3a11d1e52e22f46c0f
tootihamza <tootihamza@hotmail.fr>
2020-09-10 22:26:57 +0100

    Merging 2 documents key down event  Listener into 1

--- main.js
+++ main.js
@@ -4,10 +4,9 @@
     const btn = document.querySelector('button');
     const overlay = document.querySelector('.overlay');
 
-    /* Looping through images */
 
 
-    /* Image Browsing by click*/
+    /* Image Browsing by mouse click*/
     for(i = 1; i < 6; i++) {
         const newImage = document.createElement('img');
         newImage.setAttribute('src', "images/pic" + i + ".jpg");
@@ -19,35 +18,8 @@
             displayedImage.setAttribute("src", e.target.getAttribute("src"));
         }
     })
-    /* Image Browsing through Keys Left & Right */
 
-    document.addEventListener("keydown", function(e){
-        let srcNOW = displayedImage.getAttribute("src");
-        let newJpgNumberUp = Number(displayedImage.getAttribute("src").slice(-5, -4))+1;
-        let newJpgNumberDown = Number(displayedImage.getAttribute("src").slice(-5, -4))-1;
-        
-
-        if(e.code === "ArrowRight") {
-            if(srcNOW !== "images/pic5.jpg"){
-                displayedImage.setAttribute('src', "images/pic" + newJpgNumberUp + ".jpg");
-            }
-            else {
-                displayedImage.setAttribute("src", "images/pic1.jpg")
-            }
-        }
-        if(e.code === "ArrowLeft") {
-            if(srcNOW !== "images/pic1.jpg"){
-                displayedImage.setAttribute('src', "images/pic" + newJpgNumberDown + ".jpg");
-            }
-            else {
-                displayedImage.setAttribute("src", "images/pic5.jpg")
-            }
-        }
-
-    })
-
-
-    /* Wiring up the Darken/Lighten button */
+    /* Wiring up the Darken/Lighten button by mouse click */
 
     btn.addEventListener("click", function(){
         if(btn.classList.contains("dark")){
@@ -65,6 +37,29 @@
     })
 
     document.addEventListener("keydown", function(e){
+        let srcNOW = displayedImage.getAttribute("src");
+        let newJpgNumberUp = Number(displayedImage.getAttribute("src").slice(-5, -4))+1;
+        let newJpgNumberDown = Number(displayedImage.getAttribute("src").slice(-5, -4))-1;
+        
+        /* Image Browsing by Arrow Keys Left & Right */
+        if(e.code === "ArrowRight") {
+            if(srcNOW !== "images/pic5.jpg"){
+                displayedImage.setAttribute('src', "images/pic" + newJpgNumberUp + ".jpg");
+            }
+            else {
+                displayedImage.setAttribute("src", "images/pic1.jpg")
+            }
+        }
+        if(e.code === "ArrowLeft") {
+            if(srcNOW !== "images/pic1.jpg"){
+                displayedImage.setAttribute('src', "images/pic" + newJpgNumberDown + ".jpg");
+            }
+            else {
+                displayedImage.setAttribute("src", "images/pic5.jpg")
+            }
+        }
+
+        /* Wiring up the Darken/Lighten button with SHIFT */
         if(e.code === "ShiftLeft" || e.code === "ShiftRight")
         {
             if(btn.classList.contains("dark")){
