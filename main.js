5f163d9e9e15e4f30451b5707c19139b3a6cc7ff
tootihamza <tootihamza@hotmail.fr>
2020-09-10 02:15:06 +0100

    very small change

--- main.js
+++ main.js
@@ -1,72 +1,55 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+    const displayedImage = document.querySelector('.displayed-img');
+    const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+    const btn = document.querySelector('button');
+    const overlay = document.querySelector('.overlay');
 
-/* Looping through images */
+    /* Looping through images */
 
 
-/* Image Browsing by click*/
-for(i = 1; i < 6; i++) {
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', "images/pic" + i + ".jpg");
-    thumbBar.appendChild(newImage);
-}
-
-thumbBar.addEventListener("click", function(e){
-    if(!(e.target.classList.contains("displayed-img"))) {
-        displayedImage.setAttribute("src", e.target.getAttribute("src"));
+    /* Image Browsing by click*/
+    for(i = 1; i < 6; i++) {
+        const newImage = document.createElement('img');
+        newImage.setAttribute('src', "images/pic" + i + ".jpg");
+        thumbBar.appendChild(newImage);
     }
-})
- /* Image Browsing through Keys Left & Right */
 
-document.addEventListener("keydown", function(e){
-    let srcNOW = displayedImage.getAttribute("src");
-    let newJpgNumberUp = Number(displayedImage.getAttribute("src").slice(-5, -4))+1;
-    let newJpgNumberDown = Number(displayedImage.getAttribute("src").slice(-5, -4))-1;
-    
-
-    if(e.code === "ArrowRight") {
-        if(srcNOW !== "images/pic5.jpg"){
-            displayedImage.setAttribute('src', "images/pic" + newJpgNumberUp + ".jpg");
+    thumbBar.addEventListener("click", function(e){
+        if(!(e.target.classList.contains("displayed-img"))) {
+            displayedImage.setAttribute("src", e.target.getAttribute("src"));
         }
-        else {
-            displayedImage.setAttribute("src", "images/pic1.jpg")
+    })
+    /* Image Browsing through Keys Left & Right */
+
+    document.addEventListener("keydown", function(e){
+        let srcNOW = displayedImage.getAttribute("src");
+        let newJpgNumberUp = Number(displayedImage.getAttribute("src").slice(-5, -4))+1;
+        let newJpgNumberDown = Number(displayedImage.getAttribute("src").slice(-5, -4))-1;
+        
+
+        if(e.code === "ArrowRight") {
+            if(srcNOW !== "images/pic5.jpg"){
+                displayedImage.setAttribute('src', "images/pic" + newJpgNumberUp + ".jpg");
+            }
+            else {
+                displayedImage.setAttribute("src", "images/pic1.jpg")
+            }
         }
-    }
-    if(e.code === "ArrowLeft") {
-        if(srcNOW !== "images/pic1.jpg"){
-            displayedImage.setAttribute('src', "images/pic" + newJpgNumberDown + ".jpg");
+        if(e.code === "ArrowLeft") {
+            if(srcNOW !== "images/pic1.jpg"){
+                displayedImage.setAttribute('src', "images/pic" + newJpgNumberDown + ".jpg");
+            }
+            else {
+                displayedImage.setAttribute("src", "images/pic5.jpg")
+            }
         }
-        else {
-            displayedImage.setAttribute("src", "images/pic5.jpg")
-        }
-    }
 
-})
+    })
 
 
-/* Wiring up the Darken/Lighten button */
+    /* Wiring up the Darken/Lighten button */
 
-btn.addEventListener("click", function(){
-    if(btn.classList.contains("dark")){
-        btn.textContent = "Lighten";
-        btn.classList.remove("dark");
-        btn.classList.add("light");
-        overlay.style.backgroundColor = "rgba(0,0,0,0.5)";            
-    }
-    else {
-        btn.textContent = "Darken";
-        btn.classList.remove("light");
-        btn.classList.add("dark");
-        overlay.style.backgroundColor = "rgba(0,0,0,0.0)";  
-    }
-})
-
-document.addEventListener("keydown", function(e){
-    if(e.code === "ShiftLeft" || e.code === "ShiftRight")
-    {
+    btn.addEventListener("click", function(){
         if(btn.classList.contains("dark")){
             btn.textContent = "Lighten";
             btn.classList.remove("dark");
@@ -79,5 +62,22 @@ document.addEventListener("keydown", function(e){
             btn.classList.add("dark");
             overlay.style.backgroundColor = "rgba(0,0,0,0.0)";  
         }
-    }
-})
+    })
+
+    document.addEventListener("keydown", function(e){
+        if(e.code === "ShiftLeft" || e.code === "ShiftRight")
+        {
+            if(btn.classList.contains("dark")){
+                btn.textContent = "Lighten";
+                btn.classList.remove("dark");
+                btn.classList.add("light");
+                overlay.style.backgroundColor = "rgba(0,0,0,0.5)";            
+            }
+            else {
+                btn.textContent = "Darken";
+                btn.classList.remove("light");
+                btn.classList.add("dark");
+                overlay.style.backgroundColor = "rgba(0,0,0,0.0)";  
+            }
+        }
+    })
